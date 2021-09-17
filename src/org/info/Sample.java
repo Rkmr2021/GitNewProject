2f19e41bce6b1064b27352bafacc67f9582e24bd
Rkmr2021 <r.kmr0089@gmail.com>
2021-09-17 14:22:19 +0530

    New Project

---
+++ src/org/info/Sample.java
@@ -0,0 +1,12 @@
+package org.info;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.println("method01");
+		System.out.println("Sum of a & b :" + c);
+	}
+}
---
+++ src/org/info/Sample01.java
@@ -0,0 +1,17 @@
+package org.info;
+
+public class Sample01 {
+	public static void main(String[] args) {
+		byte age = 24;
+		System.out.println("My age is :" + age);
+		short atmPin = 2367;
+		System.out.println("My atmpin is:" + atmPin);
+		int a = 1000;
+		System.out.println("String DataType:" + (a + "100"));
+		System.out.println("Integer DataType:" + (a + 100));
+		long phn = 9709115408l;
+		System.out.println("My Phone No. :" + phn);
+
+	}
+
+}
